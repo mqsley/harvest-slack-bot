7aded2060c686048f047fd049949d91e429ccab1
Matt Quinn <mattquinn@macmini.lan>
2024-11-15 10:45:27 -0700

    updating for real firebase

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,7 +1,2 @@
-import {onRequest} from "firebase-functions/v2/https";
-import * as logger from "firebase-functions/logger";
-
-export const helloWorld = onRequest((request, response) => {
-  logger.info("Hello from Firebase Functions!");
-  response.send("Hello from Firebase Functions!");
-});
+// Start writing functions
+// https://firebase.google.com/docs/functions/typescript
